b7078238b34296d42e111631927639d445cb3520
nashslonyara52 <mgrigorev574@gmail.com>
2025-10-31 12:47:32 +0300

    zadanie 4

---
+++ Задание 4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            if (!double.TryParse(Console.ReadLine(), out double radius) || radius <= 0)
+            {
+                Console.WriteLine("Ошибка. Радиус должен быть больше 0");
+                return;
+            }
+            Circle circle = new Circle(radius);
+            circle.GetArea();
+        }
+    }
+    class Circle
+    {
+        private double radius;
+        public Circle(double r) 
+        {
+            radius = r; 
+        }
+        public void GetArea()
+        {
+            double area = radius * radius * Math.PI;
+            Console.WriteLine($"Площадь круга: {area:F2}");
+        }
+        public double Radius
+        {
+            set { radius = value; }
+            get { return radius; }
+        }
+    }
+}
