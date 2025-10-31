5acde6c96b6003ca246e2ecd31f90b7b0d4e7d36
nashslonyara52 <mgrigorev574@gmail.com>
2025-10-31 12:46:53 +0300

    zadanie 3

---
+++ Задание 3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+internal class BankAccount
+{
+    public string Owner;
+    public double Balance;
+    public BankAccount(string owner, double Balancer)
+    {
+        Owner = owner;
+        if (Balancer >= 0)
+            Balance = Balancer;
+        else
+            Balance = 0;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine(Owner + ", баланс: " + Balance + ".");
+        }
+    }
+    public void Withdraw(double amount)
+    {
+        if (amount > 0)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine(Owner + ", баланс: " + Balance + ".");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств!");
+                Console.WriteLine(Owner + ", баланс: " + Balance + ".");
+            }
+        }
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount("МегаМакс", 1000);
+        account.Deposit(500);
+        account.Withdraw(500);
+        account.Withdraw(5200); 
+    }
+}
